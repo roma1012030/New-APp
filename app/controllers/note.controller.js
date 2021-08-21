3256b90f832e119ff8da34d12cfb5508d91d1ce4
roma1012030 <76587406+roma1012030@users.noreply.github.com>
2021-08-21 13:14:50 +0600

    Update note.controller.js

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -5,46 +5,48 @@ exports.create = (req, res) => {
     // Validate request
     if(!req.body.content) {
         return res.status(400).send({
-            message: "Note content can not be empty"
+            message: "CarAd content can not be empty"
         });
     }
 
     // Create a Note
     const note = new Note({
-        title: req.body.title || "Untitled Note", 
-        content: req.body.content
+        title: req.body.title || "Untitled CarAd", 
+        price: req.body.price,
+        subject: req.body.subject,
+        Image: req.body.Image
     });
 
-    // Save Note in the database
+    // Save CarAd in the database
     note.save()
     .then(data => {
         res.send(data);
     }).catch(err => {
         res.status(500).send({
-            message: err.message || "Some error occurred while creating the Note."
+            message: err.message || "Some error occurred while creating the CarAd."
         });
     });
 };
 
-// Retrieve and return all notes from the database.
+// Retrieve and return all CarAd from the database.
 exports.findAll = (req, res) => {
     Note.find()
     .then(notes => {
         res.send(notes);
     }).catch(err => {
         res.status(500).send({
-            message: err.message || "Some error occurred while retrieving notes."
+            message: err.message || "Some error occurred while retrieving CarAd."
         });
     });
 };
 
-// Find a single note with a noteId
+// Find a single CarAd with a CarAdId
 exports.findOne = (req, res) => {
     Note.findById(req.params.noteId)
     .then(note => {
         if(!note) {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
+                message: "Note CarAd found with id " + req.params.noteId
             });            
         }
         res.send(note);
@@ -60,7 +62,7 @@ exports.findOne = (req, res) => {
     });
 };
 
-// Update a note identified by the noteId in the request
+// Update a CarAd identified by the CarAdID in the request
 exports.update = (req, res) => {
     // Validate Request
     if(!req.body.content) {
@@ -69,7 +71,7 @@ exports.update = (req, res) => {
         });
     }
 
-    // Find note and update it with the request body
+    // Find CarAd and update it with the request body
     Note.findByIdAndUpdate(req.params.noteId, {
         title: req.body.title || "Untitled Note",
         content: req.body.content
@@ -93,24 +95,24 @@ exports.update = (req, res) => {
     });
 };
 
-// Delete a note with the specified noteId in the request
+// Delete a CarAd with the specified CarAdID in the request
 exports.delete = (req, res) => {
     Note.findByIdAndRemove(req.params.noteId)
     .then(note => {
         if(!note) {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
+                message: "CarAd not found with CarAdid " + req.params.noteId
             });
         }
-        res.send({message: "Note deleted successfully!"});
+        res.send({message: "CarAd deleted successfully!"});
     }).catch(err => {
         if(err.kind === 'ObjectId' || err.name === 'NotFound') {
             return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
+                message: "CarAd not found with CarAdid " + req.params.noteId
             });                
         }
         return res.status(500).send({
-            message: "Could not delete note with id " + req.params.noteId
+            message: "Could not delete CarAd with CarAdid " + req.params.noteId
         });
     });
 };
